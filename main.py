f376a5dbf94eff80efc88e8a9ba9d1cc03e92229
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2023-10-10 18:08:59 +0530

    Writing the algorithm for the solver

--- main.py
+++ main.py
@@ -12,6 +12,41 @@ sudoku_board = [
 ]
 
 
+def valid_board(board, num, pos):
+    for i in range(len(board)):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    for i in range(box_y*3, box_y*3+3):
+        for j in range(box_x*3, box_x*3+3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
+def solve_board(board):
+
+    empty = find_empty(board)
+    if not empty:
+        return True
+    else:
+        row, col = empty
+
+    for num in range(1, 10):
+        if valid_board(board, num, (row, col)):
+            board[row][col] = num
+            if solve_board(board):
+                return True
+            board[row][col] = 0
+
+
 def print_board(board):
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
@@ -32,3 +67,12 @@ def find_empty(board):
         for j in range(len(board)):
             if board[i][j] == 0:
                 return (i, j)  # row, col
+
+    return None
+
+
+if __name__ == "__main__":
+    print_board(sudoku_board)
+    print("#############################")
+    solve_board(sudoku_board)
+    print_board(sudoku_board)
