ce84cdd56760443da510278e1dfdfb72b59c052b
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2023-10-10 19:57:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 class SudokuSolver:
     def __init__(self, board):
+        """
+        Initialize a SudokuSolver object with a 9x9 Sudoku board.
+
+        Args:
+            board (list): A 9x9 Sudoku board represented as a list of lists.
+                          Empty cells are represented by 0.
+        """
         self.board = board
 
     def is_valid(self, num, row, col):
+        """
+        Check if it's valid to place a number in a specific cell.
+
+        Args:
+            num (int): The number to be placed.
+            row (int): Row index of the cell.
+            col (int): Column index of the cell.
+
+        Returns:
+            bool: True if it's valid to place the number, False otherwise.
+        """
         for i in range(9):
             if self.board[row][i] == num or self.board[i][col] == num:
                 return False
@@ -16,6 +34,12 @@ class SudokuSolver:
         return True
 
     def solve(self):
+        """
+        Solve the Sudoku puzzle using backtracking.
+
+        Returns:
+            bool: True if a solution is found, False if no solution exists.
+        """
         empty = self.find_empty()
         if not empty:
             return True
@@ -31,6 +55,12 @@ class SudokuSolver:
         return False
 
     def find_empty(self):
+        """
+        Find the first empty cell in the Sudoku board.
+
+        Returns:
+            tuple or None: A tuple (row, col) representing the empty cell, or None if no empty cell exists.
+        """
         for i in range(9):
             for j in range(9):
                 if self.board[i][j] == 0:
@@ -38,6 +68,9 @@ class SudokuSolver:
         return None
 
     def print_board(self):
+        """
+        Print the current state of the Sudoku board in a human-readable format.
+        """
         for i in range(9):
             if i % 3 == 0 and i != 0:
                 print("- - - - - - - - - - - - ")
