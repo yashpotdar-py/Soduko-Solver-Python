d474e89cd67972fed2229bbe1073d793349d7b47
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2023-10-10 17:54:45 +0530

    Drawing board and checking empty square

---
+++ main.py
@@ -0,0 +1,34 @@
+sudoku_board = [
+    # 9x9 Sudoku board
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - ")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(f"{board[i][j]}")
+            else:
+                print(f"{board[i][j]} ", end="")
+
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 0:
+                return (i, j)  # row, col
