7f69cd159243ea40c3231373e90ae6b53c28afdb
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2023-10-10 19:54:24 +0530

    Converted the code into object oriented

--- main.py
+++ main.py
@@ -1,62 +1,53 @@
-def print_board(board):
-    for i in range(len(board)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - ")
+class SudokuSolver:
+    def __init__(self, board):
+        self.board = board
 
-        for j in range(len(board[0])):
-            if j % 3 == 0 and j != 0:
-                print(" | ", end="")
-
-            if j == 8:
-                print(f"{board[i][j]}")
-            else:
-                print(f"{board[i][j]} ", end="")
-
-
-def valid_board(board, num, pos):
-    row, col = pos
-
-    # Check row
-    if num in board[row]:
-        return False
-
-    # Check column
-    if num in [board[i][col] for i in range(9)]:
-        return False
-
-    # Check box
-    box_row = row // 3
-    box_col = col // 3
-    for i in range(box_row * 3, (box_row + 1) * 3):
-        for j in range(box_col * 3, (box_col + 1) * 3):
-            if board[i][j] == num:
+    def is_valid(self, num, row, col):
+        for i in range(9):
+            if self.board[row][i] == num or self.board[i][col] == num:
                 return False
 
-    return True
-
-
-def solve_board(board):
-    empty = find_empty(board)
-    if not empty:
+        box_row = (row // 3) * 3
+        box_col = (col // 3) * 3
+        for i in range(box_row, box_row + 3):
+            for j in range(box_col, box_col + 3):
+                if self.board[i][j] == num:
+                    return False
         return True
-    row, col = empty
 
-    for num in range(1, 10):
-        if valid_board(board, num, (row, col)):
-            board[row][col] = num
-            if solve_board(board):
-                return True
-            board[row][col] = 0
+    def solve(self):
+        empty = self.find_empty()
+        if not empty:
+            return True
+        row, col = empty
 
-    return False
+        for num in range(1, 10):
+            if self.is_valid(num, row, col):
+                self.board[row][col] = num
+                if self.solve():
+                    return True
+                self.board[row][col] = 0
 
+        return False
 
-def find_empty(board):
-    for row, row_vals in enumerate(board):
-        for col, val in enumerate(row_vals):
-            if val == 0:
-                return row, col
-    return None
+    def find_empty(self):
+        for i in range(9):
+            for j in range(9):
+                if self.board[i][j] == 0:
+                    return i, j
+        return None
+
+    def print_board(self):
+        for i in range(9):
+            if i % 3 == 0 and i != 0:
+                print("- - - - - - - - - - - - ")
+            for j in range(9):
+                if j % 3 == 0 and j != 0:
+                    print(" | ", end="")
+                if j == 8:
+                    print(self.board[i][j])
+                else:
+                    print(self.board[i][j], end=" ")
 
 
 if __name__ == "__main__":
@@ -73,9 +64,13 @@ if __name__ == "__main__":
         [0, 0, 0, 0, 8, 0, 0, 7, 9]
     ]
 
-    print_board(sudoku_board)
+    sudoku_solver = SudokuSolver(sudoku_board)
+    print("Original Sudoku Board:")
+    sudoku_solver.print_board()
     print("#############################")
-    if solve_board(sudoku_board):
-        print_board(sudoku_board)
+
+    if sudoku_solver.solve():
+        print("Solved Sudoku:")
+        sudoku_solver.print_board()
     else:
         print("No solution exists.")
